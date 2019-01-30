330ce9250c168215b715c5262fbeb94bc0494c4a
guanweisong <30776162@qq.com>
2019-01-31 00:07:25 +0800

    返回500错误具体信息

--- app/middleware/error_handler.js
+++ app/middleware/error_handler.js
@@ -8,10 +8,7 @@ module.exports = () => {
       ctx.app.emit('error', err, ctx);
 
       const status = err.status || 500;
-      // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
-      const error = status === 500 && ctx.app.config.env === 'prod'
-        ? 'Internal Server Error'
-        : err.message;
+      const error = status === 500 ? err.message : '';
 
       // 从 error 对象上读出各个属性，设置到响应中
       ctx.body = { error };
